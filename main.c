893047d3a5483dba3a3f2fb41643d77fa1e44eb9
DaveBalda <davide.baldacini@gmail.com>
2025-11-15 18:44:43 +0100

    Added braces, positive int and fora kw recognition

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 typedef enum {
   SEMI,
@@ -25,7 +27,92 @@ typedef struct {
   int value;
 } TokenLiteral;
 
+typedef struct {
+    TypeKeyword type;
+} TokenKeyword;
+
+/* --- FUNCTIONS --- */
+TokenLiteral generate_number(char *curr, FILE *file){
+    TokenLiteral token;
+    token.type = INT;
+    token.value = 0;
+
+    // Counts digits
+    while(*curr != EOF){
+        if(!isdigit(*curr))
+            break;
+        
+        token.value = (token.value * 10) + (*curr - '0');
+        *curr = fgetc(file);
+    }
+
+    return token;
+}
+
+TokenKeyword generate_keyword(char *curr, FILE *file){
+    TokenKeyword token;
+
+    char *buffer;
+    int _iter = 0;
+
+    while(*curr != EOF){
+        if(!isalpha(*curr))
+            break;
+        _iter++;
+        if(_iter == 1){
+            buffer = malloc(sizeof(char));
+            buffer[_iter-1] = *curr;
+        } else {
+            buffer = realloc(buffer, _iter * sizeof(char));
+            buffer[_iter-1] = *curr;
+        }
+        *curr = fgetc(file);
+    }
+
+    if(strcmp(buffer, "fora") == 0)
+        token.type = EXIT;
+
+    free(buffer);
+
+    return token;
+}
+
+void lexer(FILE *file){
+    char curr = fgetc(file);
+    while(curr != EOF){
+        if(curr == ';'){
+            printf("SEMI: found\n");
+            curr = fgetc(file);
+        }
+        if(curr == '('){
+            printf("OPEN_PAREN: found!\n");
+            curr = fgetc(file);
+        }
+        if(curr == ')'){
+            printf("CLOSE_PAREN: found!\n");
+            curr = fgetc(file);
+        }
+        if(isdigit(curr)){
+            TokenLiteral tok = generate_number(&curr, file);
+            printf("TOKEN LITERAL: %d\n", tok.value);
+        }
+        if(isalpha(curr)){
+            TokenKeyword tok = generate_keyword(&curr, file);
+            if(tok.type == EXIT)
+                printf("TOKEN KEYWORD: found an exit!\n");
+        }
+    }
+}
+
 int main() {
-  printf("Hello, world!\n");
-  return 0;
+    FILE *file;
+    file = fopen("test.mud", "r");
+
+    if(file == NULL){
+        printf("Error trying to open file...\n");
+        return -1;
+    }
+
+    lexer(file);
+    fclose(file);
 }
